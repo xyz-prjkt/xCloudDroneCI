76e8e6f26681a70a66705b5caae809722fd08614
Jody Yuantoro <i.xyzuan@gmail.com>
2021-07-02 08:38:47 +0700

    xCloudDroneCI: llvm-tc: didnt need any dependency install
    We already make docker which have that dependency

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,28 +1,4 @@
 #!/usr/bin/env bash
-# Main
-apt-get install -y --no-install-recommends \
-        bc \
-        bison \
-        ca-certificates \
-        clang \
-        cmake \
-        curl \
-        file \
-        flex \
-        gcc \
-        g++ \
-        git \
-        libelf-dev \
-        libssl-dev \
-        lld \
-        make \
-        ninja-build \
-        python3 \
-        texinfo \
-        xz-utils \
-        zlib1g-dev \
-        python3
-
 # Use xRageTC build script as LLVM Build Script.
 git clone https://github.com/xyz-prjkt/xRageTC $(pwd)/llvm-tc -b llvm-tc_template
 cd $(pwd)/llvm-tc
