5c119d192196db2603d1f97dc364ab91da69e775
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-06 13:31:59 +0700

    xCloudDroneCI: xKernelCompiler: initial.

---
+++ kernel_build.sh
@@ -0,0 +1,119 @@
+#!/usr/bin/env bash
+#
+# Copyright (C) 2021 a xyzprjkt property
+#
+
+# Needed Secret Variable
+# KERNEL_NAME | Your kernel name
+# KERNEL_SOURCE | Your kernel link source
+# KERNEL_BRANCH  | Your needed kernel branch if needed with -b. eg -b eleven_eas
+# DEVICE_CODENAME | Your device codename
+# DEVICE_DEFCONFIG | Your device defconfig eg. lavender_defconfig
+# BUILD_USER | Your username
+# BUILD_HOST | Your hostname
+
+echo "Downloading few Dependecies . . ."
+# Kernel Sources
+git clone --depth=1 $KERNEL_SOURCE $KERNEL_BRANCH $DEVICE_CODENAME
+git clone --depth=1 https://github.com/xyz-prjkt/xRageTC_build xRageTC # xRageTC set as Clang Default
+
+# Main Declaration
+KERNEL_ROOTDIR=$(pwd)/$DEVICE_CODENAME # IMPORTANT ! Fill with your kernel source root directory.
+DEVICE_DEFCONFIG=$DEVICE_DEFCONFIG # IMPORTANT ! Declare your kernel source defconfig file here.
+CLANG_ROOTDIR=$(pwd)/xRageTC # IMPORTANT! Put your clang directory here.
+export KBUILD_BUILD_USER=$BUILD_USER # Change with your own name or else.
+export KBUILD_BUILD_HOST=$BUILD_HOST # Change with your own hostname.
+
+# Main Declaration
+CLANG_VER="$("$CLANG_ROOTDIR"/bin/clang --version | head -n 1 | perl -pe 's/\(http.*?\)//gs' | sed -e 's/  */ /g' -e 's/[[:space:]]*$//')"
+LLD_VER="$("$CLANG_ROOTDIR"/bin/ld.lld --version | head -n 1)"
+export KBUILD_COMPILER_STRING="$CLANG_VER with $LLD_VER"
+IMAGE=$(pwd)/$DEVICE_CODENAME/out/arch/arm64/boot/Image.gz-dtb
+DATE=$(date +"%F-%S")
+START=$(date +"%s")
+
+# Checking environtment
+# Warning !! Dont Change anything there without known reason.
+function check() {
+echo ================================================
+echo xKernelCompiler
+echo version : rev1.5 - gaspoll
+echo ================================================
+echo BUILDER NAME = ${KBUILD_BUILD_USER}
+echo BUILDER HOSTNAME = ${KBUILD_BUILD_HOST}
+echo DEVICE_DEFCONFIG = ${DEVICE_DEFCONFIG}
+echo TOOLCHAIN_VERSION = ${KBUILD_COMPILER_STRING}
+echo CLANG_ROOTDIR = ${CLANG_ROOTDIR}
+echo KERNEL_ROOTDIR = ${KERNEL_ROOTDIR}
+echo ================================================
+}
+
+# Telegram
+export BOT_MSG_URL="https://api.telegram.org/bot$TG_TOKEN/sendMessage"
+
+tg_post_msg() {
+  curl -s -X POST "$BOT_MSG_URL" -d chat_id="$TG_CHAT_ID" \
+  -d "disable_web_page_preview=true" \
+  -d "parse_mode=html" \
+  -d text="$1"
+
+}
+
+# Post Main Information
+tg_post_msg "<b>xKernelCompiler</b>%0ABuilder Name : <code>${KBUILD_BUILD_USER}</code>%0ABuilder Host : <code>${KBUILD_BUILD_HOST}</code>%0ADevice Defconfig: <code>${DEVICE_DEFCONFIG}</code>%0AClang Version : <code>${KBUILD_COMPILER_STRING}</code>%0AClang Rootdir : <code>${CLANG_ROOTDIR}</code>%0AKernel Rootdir : <code>${KERNEL_ROOTDIR}</code>"
+
+# Compile
+tg_post_msg "<b>xKernelCompiler:</b><code>Compilation has started"
+cd ${KERNEL_ROOTDIR}
+make -j$(nproc) O=out ARCH=arm64 ${DEVICE_DEFCONFIG}
+make -j$(nproc) ARCH=arm64 O=out \
+    CC=${CLANG_ROOTDIR}/bin/clang \
+    AR=${CLANG_ROOTDIR}/bin/llvm-ar \
+  	NM=${CLANG_ROOTDIR}/bin/llvm-nm \
+  	OBJCOPY=${CLANG_ROOTDIR}/bin/llvm-objcopy \
+  	OBJDUMP=${CLANG_ROOTDIR}/bin/llvm-objdump \
+    STRIP=${CLANG_ROOTDIR}/bin/llvm-strip \
+    CROSS_COMPILE=${CLANG_ROOTDIR}/bin/aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=${CLANG_ROOTDIR}/bin/arm-linux-gnueabi-
+
+   if ! [ -a "$IMAGE" ]; then
+	finerr
+	exit 1
+   fi
+  
+  git clone --depth=1 $ANYKERNEL AnyKernel
+	cp $IMAGE AnyKernel
+}
+
+# Push kernel to channel
+function push() {
+    cd AnyKernel
+    ZIP=$(echo *.zip)
+    curl -F document=@$ZIP "https://api.telegram.org/bot$TG_TOKEN/sendDocument" \
+        -F chat_id="$TG_CHAT_ID" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="Compile took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>$DEVICE_CODENAME</b> | <b>${KBUILD_COMPILER_STRING}</b>"
+}
+# Fin Error
+function finerr() {
+    curl -s -X POST "https://api.telegram.org/bot$TG_TOKEN/sendMessage" \
+        -d chat_id="$TG_CHAT_ID" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+
+# Zipping
+function zipping() {
+    cd AnyKernel || exit 1
+    zip -r9 $KERNEL_NAME-$DEVICE_CODENAME-${DATE}.zip *
+    cd ..
+}
+check
+compile
+zipping
+END=$(date +"%s")
+DIFF=$(($END - $START))
+push
--- llvm-tc_build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/xRageTC $(pwd)/llvm-tc -b llvm-tc_template
-cd $(pwd)/llvm-tc
-bash build-tc.sh
