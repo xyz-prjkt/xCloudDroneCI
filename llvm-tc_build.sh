3a106d93da03b8a1f5bba6bd7e3aa371978933ec
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-07-06 16:51:44 +0700

    xCloudDroneCI: llvm-tc: update repo remote

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/xRageTC $(pwd)/llvm-tc -b llvm-tc_template
-cd $(pwd)/llvm-tc
+git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
+cd $(pwd)/llvmTC
 bash build-tc.sh
