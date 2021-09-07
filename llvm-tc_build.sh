d23dff30baea391b80dcb6c9ac30aa558dba10c5
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-09-07 11:52:58 +0700

    xCloudDroneCI: llvm-tc: change branch remote

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc-lto_x86
+git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc_lto-x86
 cd $(pwd)/llvmTC
 bash build-tc.sh
