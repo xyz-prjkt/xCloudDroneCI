daaec5f6b15fff17ced73f8dcba3b0eb5f4033f7
Jody Yuantoro <xyzuannihboss@gmail.com>
2021-10-19 17:55:58 +0700

    xCloudDroneCI: llvm-tc: init x86

--- llvm-tc_build.sh
+++ llvm-tc_build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Use xRageTC build script as LLVM Build Script.
-git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc
+git clone https://github.com/xyz-prjkt/llvmTC $(pwd)/llvmTC -b llvm-tc_x86
 cd $(pwd)/llvmTC
 bash build-tc.sh
